a11bcf898acfabb44c7574fda55931f788b1d4fd
ibrahim <ibrahimrahman2806@gmail.com>
2025-12-09 16:52:53 +0000

    home page layout

--- frontend-finance-tracker-v2/src/pages/Home.jsx
+++ frontend-finance-tracker-v2/src/pages/Home.jsx
@@ -1,11 +1,12 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 import { showLogout } from "../utils/toast";
 
 export default function Home() {
   const { isAuthenticated, isLoading } = useAuth0();
+  const [hoveredItem, setHoveredItem] = useState(false);
   
-  // logout alert
   useEffect(() => {
     if (!isLoading && !isAuthenticated) {
       showLogout("You've been logged out.");
@@ -14,9 +15,63 @@ export default function Home() {
 
 
   return (
-    <section>
-      <h1>Home Page</h1>
-      <p>Welcome to the Finance Tracker!</p>
-    </section>
+    <div style={{
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+    }}>
+
+
+      <div style={{ minHeight: "90vh"}}>
+        <h1>Home Page</h1>
+      </div>
+    
+      <div style={{ borderBottom: "2px solid white", width: "80%", margin: "20px" }}></div>
+      <div style={{ display: "grid", gridTemplateColumns: "repeat(2, 1fr)", alignItems: "center", justifyContent: "center" }}>
+        <Link to="/dashboard" style={{
+          display: "flex",
+          flexDirection: "column",
+          backgroundColor: hoveredItem === "finance-tracker" ? "#666" : "#333",
+          borderRadius: "10px",
+          padding: "0 0 3rem 0",
+          width: "80%", 
+          alignItems: "center",
+          justifyContent: "center",
+          minHeight: "50vh",
+          minWidth: "15vw",
+          margin: "3rem",
+        }}
+        onMouseEnter={() => setHoveredItem("finance-tracker")}
+        onMouseLeave={() => setHoveredItem(false)}
+        >
+          <h2 style={{fontSize: "1rem"}}>Finance Tracker</h2>
+
+        </Link>
+
+        <Link to="http://localhost:5174/" target="blank" style={{
+          display: "flex",
+          flexDirection: "column",
+          backgroundColor: hoveredItem === "news-feed" ? "#666" : "#333",
+          borderRadius: "10px",
+          padding: "0 0 3rem 0",
+          width: "80%", 
+          alignItems: "center",
+          justifyContent: "center",
+          minHeight: "50vh",
+          minWidth: "15vw",
+          margin: "3rem",
+        }}
+        onMouseEnter={() => setHoveredItem("news-feed")}
+        onMouseLeave={() => setHoveredItem(false)}
+        >
+          <h2 style={{fontSize: "1rem"}}>Stock News Feed</h2>
+
+        </Link>
+
+      </div>
+
+    </div>
+
   );
 }
